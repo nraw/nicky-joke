50140314a217479c4c50d224d63b3a44fe2cb9d9
Andrej-Marsic <andrej_marsic@mckinsey.com>
2022-03-29 18:33:08 +0200

    texto

--- run.py
+++ run.py
@@ -1,9 +1,24 @@
 import os
+from datetime import date
 from pathlib import Path
 
-data = os.environ.get(
-    "data", "No idea what's happening, but this ain't working"
-)
+data = os.environ.get("data")
+if data:
+    joke = data.get("1604150654")
+    today = date.today().strftime("%Y-%m-%d")
+    cool_date = data.get("824302638", today)
+    date.now
+    full_text = f"""
+    The joke that was made
+    ```
+    {joke}
+    ```
+    on {cool_date}
+    """
+else:
+    full_text = (
+        "No idea what's happening, but this ain't working correctly. Blame kaki."
+    )
+
 print(data)
-print(os.environ)
-Path("what.md").write_text(data)
+Path("what.md").write_text(full_text)
