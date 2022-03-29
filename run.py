df5517195d9ee1f1964f92ccfd2c79aadf3aa745
Andrej-Marsic <andrej_marsic@mckinsey.com>
2022-03-29 18:08:53 +0200

    please?

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 import os
 from pathlib import Path
 
-client_payload = os.environ.get(
-    "client_payload", "No idea what's happening, but this ain't working"
+data = os.environ.get(
+    "data", "No idea what's happening, but this ain't working"
 )
-print(client_payload)
+print(data)
 print(os.environ)
-Path("what.md").write_text(client_payload)
+Path("what.md").write_text(data)
