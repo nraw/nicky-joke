fec9b3fd8bea4eb510d0ebf62aa1e72586ae2a13
Andrej-Marsic <andrej_marsic@mckinsey.com>
2022-03-29 17:26:14 +0200

    writing payload to file

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
-from pathlib import Path
 import os
+from pathlib import Path
 
-client_payload = os.environ.get('client_payload')
+client_payload = os.environ.get(
+    "client_payload", "No idea what's happening, but this ain't working"
+)
 print(client_payload)
 print(os.environ)
-Path('what.md').write_text('# hi')
+Path("what.md").write_text(client_payload)
